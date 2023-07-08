08deeca6924855006c1abf123f46b08008dcae73
rahuldhanotia3 <rahuldhanotia3@gmail.com>
2023-07-08 18:57:43 +0530

    First Commit

---
+++ ProjectAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using ProjectAPI.DbData;
+using ProjectAPI.Models;
+
+namespace ProjectAPI.Controllers
+{
+    [ApiController]
+    [Route("api/Employee")]
+    public class EmployeeController : Controller
+    {
+        public EmployeeDbContext employeeDbContext;
+        public EmployeeController(EmployeeDbContext context)
+        {
+            this.employeeDbContext = context;
+        }
+        
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Employee> result;
+            
+            result = employeeDbContext.Employees.FromSqlRaw("EmployeeGetAll").ToList();
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Employee e)
+        {
+            var result = employeeDbContext.Database.ExecuteSqlRaw("EXEC EmployeeCreate @id, @name, @dept",
+                new SqlParameter("@id", e.Id),
+                new SqlParameter("@name", e.Name),
+                new SqlParameter("@dept", e.Department));
+
+            if (result > 0)
+                return Ok(e);
+            else
+                return BadRequest(-1);
+        }
+    }
+}
---
+++ ProjectAPI/DbData/EmployeeDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjectAPI.Models;
+
+namespace ProjectAPI.DbData
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
